94b07de3131bdfa4c72c78110e74b826c6622e3b
asif-m <mohd.asif55@gmail.com>
2024-02-01 18:18:04 -0800

    Title

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,6 +6,7 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang="en">
         <head>
+          <title>Hifz</title>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.png" />
