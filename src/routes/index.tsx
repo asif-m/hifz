8d197f30d138440ec309b093c38d262ad386460c
asif-m <mohd.asif55@gmail.com>
2024-02-02 10:14:49 -0800

    Material UI Integration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,38 @@
 // @refresh reload
 import { createSignal } from "solid-js";
 
+import { Box, Button, FormControl, InputLabel, MenuItem, Select } from "@suid/material";
+
 export default function Index() {
   const [count, setCount] = createSignal(0);
+  const [age, setAge] = createSignal("");
 
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
   return (
     <main>
       <h1>Home Page</h1>
       <button class="increment" onClick={() => setCount(count() + 1)}>
         Clicks: {count()}
       </button>
+
+      <Button variant="contained">Hello world</Button>
+
+      <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel>Age</InputLabel>
+        <Select
+          value={age()}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
     </main>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
+import solidPlugin from "vite-plugin-solid";
+import suidPlugin from "@suid/vite-plugin";
 
-export default defineConfig({ start: { ssr: false } });
+export default defineConfig({ 
+    start: { ssr: false } ,
+    //plugins: [suidPlugin(), solidPlugin()],
+    // build: {
+    //     target: "esnext",
+    // },
+});
