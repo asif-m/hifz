e58f8829e9453a22f3458afc47d27a738c347798
asif-m <mohd.asif55@gmail.com>
2024-02-02 11:56:42 -0800

    Adding regions

--- src/components/wavesurfer-wrapper.component.tsx
+++ src/components/wavesurfer-wrapper.component.tsx
@@ -1,23 +1,24 @@
 // @refresh reload
-import { createEffect, createSignal } from "solid-js";
-
-import { Box, Button, FormControl, InputLabel, MenuItem, Select } from "@suid/material";
-import WaveSurfer from 'wavesurfer.js'
+import { createEffect } from "solid-js";
+import WaveSurfer from 'wavesurfer.js';
+import ZoomPlugin from '../../node_modules/wavesurfer.js/dist/plugins/zoom.esm.js';
+import RegionsPlugin from '../../node_modules/wavesurfer.js/dist/plugins/regions.esm.js';
 
 import { IReciterTimeStamp } from "~/models/ayah-info-interface";
 
 export default function WavesurferWrapperComponent() {
   
-  const timeStamps: IReciterTimeStamp = [{
+  const timeStamps: Array<IReciterTimeStamp> = [{
     timestampFrom: 0,
-    timestampTo: 50,
+    timestampTo: 2,
   },
   {
-    timestampFrom: 51,
-    timestampTo: 150,
+    timestampFrom: 3,
+    timestampTo: 4,
   }
 ]
 
+// Give regions a random color when they are created
 const random = (min, max) => Math.random() * (max - min) + min
 const randomColor = () => `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`
 
@@ -25,12 +26,34 @@ const randomColor = () => `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0
   createEffect(()=>{
     const ws = WaveSurfer.create({
         container: '#waveform',
-        waveColor: 'rgb(200, 0, 200)',
-        progressColor: 'rgb(100, 0, 100)',
+        waveColor: 'rgb(0, 200, 200)',
+        progressColor: 'rgb(0, 100, 100)',
         url: '/audio/Sameer Nass/1-سورة الفاتحة.mp3',
+        //minPxPerSec: 100,
       })
-      // Initialize the Regions plugin
-
+      // Initialize the Zoom plugin
+      ws.registerPlugin(
+        ZoomPlugin.create({
+          // the amount of zoom per wheel step, e.g. 0.5 means a 50% magnification per scroll
+          scale: 0.5,
+          // Optionally, specify the maximum pixels-per-second factor while zooming
+          maxZoom: 100,
+        }),
+      )
+      const wsRegions = ws.registerPlugin(RegionsPlugin.create())
+      ws.on('decode', () => {
+        // Regions
+        timeStamps.forEach((timeStamp, index) => {
+          wsRegions.addRegion({
+            start: timeStamp.timestampFrom,
+            end: timeStamp.timestampTo,
+            content: `${index+1}`,
+            color: randomColor(),
+            drag: false,
+            resize: true,
+          })
+        })
+      })
   },[])
 
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from "@solidjs/start/config";
-import solidPlugin from "vite-plugin-solid";
-import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({ 
-    start: { ssr: false } ,
-    //plugins: [suidPlugin(), solidPlugin()],
-    // build: {
-    //     target: "esnext",
-    // },
+    start: { ssr: false } 
 });
