2601115d43d3107cdf1c7979b4677ab0d052a6df
asif-m <mohd.asif55@gmail.com>
2024-02-03 08:39:40 -0800

    renaming surah folder and adding currently playing

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,18 +1,21 @@
 import { createSignal, createContext, useContext } from "solid-js";
 import type { ComponentProps } from "~/utils/component-type";
 import type { Accessor, Setter } from 'solid-js';
+import { IAyah } from "~/models/ayah-info-interface";
 
 const StoreContext = createContext();
 
 export interface IStoreData {
     verseNumber: number,
-    chapterNumber: number
+    chapterNumber: number,
+    currentPlaying: IAyah | null
 }
 
 export function getInitialStoreData(): IStoreData {
     return {
         verseNumber: 0,
-        chapterNumber: 0
+        chapterNumber: 0,
+        currentPlaying: null
     }
 }
 
@@ -21,6 +24,8 @@ export interface IStoreUseContextData {
     setVerseNumber: Setter<number>
     chapterNumber: Accessor<number>
     setChapterNumber: Setter<number>
+    currentPlaying: Accessor<IAyah | null>
+    setCurrentPlaying: Setter<IAyah | null>
 }
 
 
@@ -28,12 +33,15 @@ export function StoreProvider(props: ComponentProps<IStoreData>) {
 
     const [verseNumber, setVerseNumber] = createSignal(props.verseNumber);
     const [chapterNumber, setChapterNumber] = createSignal(props.chapterNumber);
+    const [currentPlaying, setCurrentPlaying] = createSignal(props.currentPlaying);
 
     const value = {
         verseNumber,
         setVerseNumber,
         chapterNumber,
-        setChapterNumber
+        setChapterNumber,
+        currentPlaying,
+        setCurrentPlaying
     };
 
     return (
