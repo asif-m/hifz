519379771cad240c10f931a10c03bb9ffd96607f
asif-m <mohd.asif55@gmail.com>
2024-02-09 07:13:15 -0800

    Correcting import

--- src/components/ayah-tracker.tsx
+++ src/components/ayah-tracker.tsx
@@ -1,8 +1,7 @@
-import { Save } from "@suid/icons-material";
-import { IconButton } from "@suid/material";
-import { For, createEffect, createSignal } from "solid-js";
+import { For, createSignal } from "solid-js";
 import { useStore } from "~/store/store";
-import { CLocalStorageHelper } from "~/utils/localstorage-helper";
+import { IconButton } from "@suid/material";
+import Save from "@suid/icons-material/Save";
 
 interface IAyahDataInLocalStorageIndividual { chapterNumber: number, verseNumber: number, timestampFrom: number, timestampTo: number }
 interface IAyahDataInLocalStorage {
@@ -13,43 +12,12 @@ export default function AyahTrackerComponent() {
     const { pageData, pageNumber } = useStore();
     const [audioTimeStamps, setAudioTimeStamps] = createSignal<Array<IAyahDataInLocalStorageIndividual>>([]);
 
-
-    createEffect(() => {
-        const page = pageNumber();
-        if (page === 0) {
-            return;
-        }
-        const key = `Quran Page Audio : ${page}`;
-        const { data = [] } = CLocalStorageHelper.read(key) as IAyahDataInLocalStorage;
-        const initialData: Array<IAyahDataInLocalStorageIndividual> = [];
-        pageData().ayahs.forEach((ayah) => {
-            const index = data.findIndex((a) => a.chapterNumber === ayah.chapterNumber && a.verseNumber === ayah.verseNumber);
-            let timestampFrom = ayah.reciterTimestamps["Shaykh Samir al-Nass"].timestampFrom;
-            let timestampTo = ayah.reciterTimestamps["Shaykh Samir al-Nass"].timestampTo;
-            if (index !== -1) {
-                timestampFrom = data[index].timestampFrom;
-                timestampTo = data[index].timestampTo;
-            }
-            initialData.push({
-                chapterNumber: ayah.chapterNumber,
-                verseNumber: ayah.verseNumber,
-                timestampFrom: timestampFrom,
-                timestampTo: timestampTo,
-            })
-        });
-        setAudioTimeStamps(() => initialData)
-    })
-
-    function onSave() {
-        //CLocalStorageHelper.update(key,audioTimeStamps )
-    }
-
-
+     function onSave(){}
     return (<div>
         <For each={audioTimeStamps()}>{ayah =>
             <div>{`${ayah.chapterNumber}:${ayah.verseNumber}  (${ayah.timestampFrom}-${ayah.timestampTo})`}</div>}</For>
         <IconButton aria-label="Save" onclick={onSave}>
             <Save />
-        </IconButton>
+        </IconButton> 
     </div>)
 }
