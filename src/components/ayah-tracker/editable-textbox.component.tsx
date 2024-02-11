2ad76e499141cc924dff27e587569e7706349cfd
asif-m <mohd.asif55@gmail.com>
2024-02-11 10:16:28 -0800

    Restricting to numbers

--- src/components/ayah-tracker/editable-textbox.component.tsx
+++ src/components/ayah-tracker/editable-textbox.component.tsx
@@ -1,42 +1,34 @@
 import { TextField } from "@suid/material";
-import { Show, createSignal } from "solid-js";
 import { AudioPlayerState } from "~/models/audio-player-state";
 import { useStore } from "~/store/store";
 
 export default function EditableTextboxControlsComponent(props: { value: number, isFrom: boolean, onChange: (value: number) => void }) {
     const { value, onChange, isFrom } = props;
     const { setAudioPlayerState, setAudioCurrentTime, setAudioTimetrackAutoUpdate } = useStore();
-    const [editing, setEditing] = createSignal(false);
-    function getTime(v: string) {
-        return parseFloat(parseFloat(v).toPrecision(1));
+    
+    function onValueChange(v:string){
+        try{
+            const time = parseFloat(parseFloat(v).toPrecision(1));
+            onChange(time);
+            setAudioCurrentTime(() => time);
+        }catch(e){
+
+        }
     }
     return (
         <div>
-            <Show when={editing()}>
-                <TextField
-                    type="number"
+         <TextField
                     value={value}
-                    inputProps={{ style: { "font-size": "8px" } }} // font size of input text
+                    inputProps={{ style: { "font-size": "8px" }}} // font size of input text
                     InputLabelProps={{ style: { "font-size": "8px" } }} // font size of input label
                     variant="standard"
                     onFocus={() => {
                         setAudioPlayerState(() => AudioPlayerState.stopped);
+                        setAudioTimetrackAutoUpdate(() => false);
                         setAudioCurrentTime(() => value);
                     }}
-                    onBlur={() => setEditing(() => false)}
-                    onChange={(event, v) => {
-                        const time = getTime(v);
-                        onChange(time);
-                        setAudioCurrentTime(() => time);
-                    }}
+                    onChange={(event, v) => {onValueChange(v)}}
                 />
-            </Show>
-            <Show when={!editing()}>
-                <div role="button" onClick={() => {
-                    setAudioTimetrackAutoUpdate(() => false);
-                    setEditing(() => true)
-                }}>{value}</div>
-            </Show>
         </div>
     )
 }
