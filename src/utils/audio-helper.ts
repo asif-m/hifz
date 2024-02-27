823f1cf9efb5dac97372290ee7e175226ad4e4a4
asif-m <mohd.asif55@gmail.com>
2024-02-27 08:07:56 -0800

    finding silent region update

--- src/utils/audio-helper.ts
+++ src/utils/audio-helper.ts
@@ -1,9 +1,11 @@
-export function extractSilenceRegions(audioData, duration) {
+import { IReciterTimeStamp } from "~/models/ayah-info-interface"
+
+export function extractSilenceRegions(audioData:Float32Array, duration:number) {
     const minValue = 0.01
     const minSilenceDuration = 0.1
     const mergeDuration = 0.2
     const scale = duration / audioData.length
-    const silentRegions = []
+    const silentRegions:Array<IReciterTimeStamp> = []
   
     // Find all silent regions longer than minSilenceDuration
     let start = 0
@@ -20,36 +22,46 @@ export function extractSilenceRegions(audioData, duration) {
         isSilent = false
         if (scale * (end - start) > minSilenceDuration) {
           silentRegions.push({
-            start: scale * start,
-            end: scale * end,
+            timestampFrom: scale * start,
+            timestampTo: scale * end,
           })
         }
       }
     }
-  
+
     // Merge silent regions that are close together
     const mergedRegions = []
     let lastRegion = null
     for (let i = 0; i < silentRegions.length; i++) {
-      if (lastRegion && silentRegions[i].start - lastRegion.end < mergeDuration) {
-        lastRegion.end = silentRegions[i].end
+      if (lastRegion && silentRegions[i].timestampFrom - lastRegion.timestampTo < mergeDuration) {
+        lastRegion.timestampTo = silentRegions[i].timestampTo
       } else {
         lastRegion = silentRegions[i]
         mergedRegions.push(lastRegion)
       }
     }
-  
-    // Find regions that are not silent
-    const regions = []
-    let lastEnd = 0
-    for (let i = 0; i < mergedRegions.length; i++) {
-      regions.push({
-        timestampFrom: lastEnd,
-        timestampTo: mergedRegions[i].start,
-      })
-      lastEnd = mergedRegions[i].end
-    }
-  
-    return regions
+    return mergedRegions;
   }
-  
+
+  export function findClosestSilentRegion(silentRegions:Array<IReciterTimeStamp>, timeStamp: number){
+    let closestRegion = silentRegions[0];
+    let prevDistance = distanceFromRegion(silentRegions[0],timeStamp);
+    for(let i=1;i<silentRegions.length;i++){
+      const currDistance = distanceFromRegion(silentRegions[i],timeStamp)
+      if(prevDistance>currDistance){
+        prevDistance = currDistance;
+        closestRegion = silentRegions[i];
+      }
+    }
+    return {region: closestRegion, middle:(closestRegion.timestampFrom+closestRegion.timestampTo)/2}
+  }
+
+  function distanceFromRegion(region: IReciterTimeStamp, timeStamp:number){
+    if(timeStamp > region.timestampTo){
+      return (timeStamp - region.timestampTo);
+    }
+    if(timeStamp<region.timestampFrom){
+      return (region.timestampFrom - timeStamp)
+    }
+    return Math.abs(region.timestampFrom - timeStamp);
+  }
