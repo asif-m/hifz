fbef4e8069c156e51b905bc8845b2e7781c2a2d3
asif-m <mohd.asif55@gmail.com>
2024-02-28 20:45:12 -0800

    Adding declarations

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="@solidjs/start/env" />
+declare module "wavesurfer.js/dist/plugins/zoom.esm.js";
+declare module "wavesurfer.js/dist/plugins/regions.esm.js";
+declare module "wavesurfer.js/dist/plugins/timeline.esm.js";
